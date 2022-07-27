d3bb26423803f1285b23122bed2fdd8f9a6bb156
ilyass <elmofid.ilyass@gmail.com>
2022-07-27 11:41:44 +0100

    final update

--- src/main/java/org/sid/invetoryservice/entities/Product.java
+++ src/main/java/org/sid/invetoryservice/entities/Product.java
@@ -21,3 +21,5 @@ public class Product {
     private String name ;
     private double price ;
 }
+
+
--- src/main/java/org/sid/invetoryservice/repositories/ProductRepository.java
+++ src/main/java/org/sid/invetoryservice/repositories/ProductRepository.java
@@ -7,3 +7,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface ProductRepository extends JpaRepository<Product , Long> {
 }
+
